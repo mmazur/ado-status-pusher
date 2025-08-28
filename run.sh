5ba511eb022d5e982802cd5d03949e8c2221b5b8
Mariusz Mazur <mmazur@redhat.com>
2025-08-28 11:35:54 +0200

    Use dedicated dirs for tmp files

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ if [ -z "$SLACK_URL" ]; then
     exit 1
 fi
 
+mkdir -f builds var 2>/dev/null
+
 while true; do
     echo -ne "\n\n"
     date
@@ -12,18 +14,17 @@ while true; do
     curTS="$(date -u +%Y-%m-%d-%H-%M-%S)"
     cur="builds/$curTS.json"
 
-    [ ! -f "prev.txt" ] && echo "$curTS" > prev.txt
-    prevTS="$(head -n 1 prev.txt)"
+    prevTS="$(head -n 1 var/prev.txt)"
     prev="builds/$prevTS.json"
 
     az pipelines build list --definition-ids 428766 428757 428756 > $cur && \
-    ./compare.py $prev $cur > msg.txt
+    ./compare.py $prev $cur > var/msg.txt
 
-    if [ -s msg.txt ]; then
-        cat msg.txt && \
-        jq -Rsa --raw-input '{"msg": .}' msg.txt > msg.json && \
-        curl -X POST -H "Content-Type: application/json" -d "@msg.json" "$SLACK_URL" && \
-        echo $curTS > prev.txt && \
+    if [ -s var/msg.txt ]; then
+        cat var/msg.txt && \
+        jq -Rsa --raw-input '{"msg": .}' var/msg.txt > var/msg.json && \
+        curl -X POST -H "Content-Type: application/json" -d "@var/msg.json" "$SLACK_URL" && \
+        echo $curTS > var/prev.txt && \
         rm -f $prev
     else
         rm -rf $cur
